ebc0695b3654f1fdea475e4a4b259b18a3caf16e
Jason <jasonrigden@gmail.com>
2016-11-12 13:15:38 -0800

    initial models

--- wherenottoeat/eatery/models.py
+++ wherenottoeat/eatery/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    Address = models.CharField(max_length=50)
+    Business_ID = models.CharField(max_length=50, unique=True)
+    City = models.CharField(max_length=50)
+    Description = models.CharField(max_length=50)
+    Location = models.PointField(null=True, blank=True)
+    Name = models.CharField(max_length=50)
+    Program_Identifier = models.CharField(max_length=50, unique=True)
+    Zip_Code = models.CharField(max_length=5)
+
+
+class Inspection(models.Model):
+    Inspection_Date = models.DateField()
+    Inspection_Closed_Business = model.BooleanField()
+    Inspection_Result = models.CharField(max_length=50)
+    Inspection_Score = models.IntegerField()
+    Inspection_Serial_Num = models.CharField(max_length=50, unique=True)
+    Inspection Type = models.CharField(max_length=50)
+    Restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE,)
+
+
+class Violation(models.Model):
+    Violation_Description = models.CharField(max_length=150)
+    Violation_Points = models.IntegerField()
+    Violation_Record_ID = models.CharField(max_length=50, unique=True)
+    Violation_Type = models.CharField(max_length=50)
+
+    Inspection = models.ForeignKey('Inspection', on_delete=models.CASCADE,)
